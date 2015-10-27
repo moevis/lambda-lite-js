d2ff03521b8b86182ecdcb46a177495a7e7ee194
moe <moevery@gmail.com>
2015-10-27 09:19:57 +0800

    parttern matching support array

--- test/pattern.test.js
+++ test/pattern.test.js
@@ -1,49 +1,22 @@
-var pattern = require('../lib/pattern.js').Pattern;
+var Pattern = require('../lib/pattern.js').Pattern;
 
-var PatternMatching = function (options) {
-    var opts = [];
-    options.forEach(function (rule) {
-        var args = [];
-        for (var i = 0, length = rule.length - 1; i < length; i ++) {
-            args.push(new pattern.unit(rule[i]));
-        }
-        args.push(rule[rule.length - 1]);
-        //if (args.length > 1) {
-            opts.push(args);
-        //}
-    });
-    //console.log(opts);
-    return function () {
-        var result;
-        var args = arguments;
-        opts.forEach(function (rule) {
-            var flag = true;
-            for (var i = 0, length = rule.length - 1; i < length; i ++) {
-                //rule.log(opt[i]);
-                if (!rule[i].expect(args[i])) {
-                    flag = false;
-                }
-            }
-            if (flag) {
-                var func = rule[length];
-                result = func.apply(null, args);
-            }
-        });
-        return result;
-    }
-};
+var PatternMatching = Pattern.PatternMatching;
 
 var test = PatternMatching([
     [Number, function (n) {
-        console.log(n);
         return 'number';
     }],
     [String, function (s) {
-        console.log(s);
         return 'string';
+    }],
+    ['$', function (S) {
+        return '$';
+    }],
+    [[], function (x) {
+        return 'any';
     }]
 ]);
 
 
-console.log(test(1));
-console.log(test('1'));
+//console.log(test(1));
+console.log(test([]));
