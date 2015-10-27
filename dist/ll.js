5d3364df8d929c30aa4e2f27ceb66a489b64b470
moe <moevery@gmail.com>
2015-10-27 23:03:23 +0800

    add operator '!!'
    [1,2,3] !! 1 == 2

--- dist/ll.js
+++ dist/ll.js
@@ -1,4 +1,4 @@
-/*! PROJECT_NAME - v0.1.0 - 2015-10-26
+/*! PROJECT_NAME - v0.1.0 - 2015-10-27
 * http://icymorn.github.io/lambda-lite-js/
 * Copyright (c) 2015 ICYMORN; Licensed MIT */
 var ll = {
@@ -164,6 +164,7 @@ define('./lex', ['./util', './token', './node'], function (exports) {
         '/'          : 110,
         '%'          : 110,
         '++'         : 110,
+        '!!'         : 110,
         ':'          : 110
     };
 
@@ -371,6 +372,8 @@ define('./lex', ['./util', './token', './node'], function (exports) {
                             index ++;
                         }
                     }
+                } else if (ch === '!'){
+                    index ++;
                 }
             }
         } else if (fstCh === '&') {
@@ -619,8 +622,7 @@ define('./lex', ['./util', './token', './node'], function (exports) {
         var expre1 = genTopLevelNode();
         consumeKeyword('else');
         var expre2 = genTopLevelNode();
-        var node = new Node.ifConditionNode(cond, expre1, expre2);
-        return node;
+        return new Node.ifConditionNode(cond, expre1, expre2);
     }
 
     function genDefineNode () {
@@ -870,6 +872,9 @@ define('./node', [],function (exports) {
                 return [].concat(left, right);
             }
 
+        } else if (this.operator === '!!') {
+            right = this.right.getValue(scope);
+            return left[right];
         }
     };
 
@@ -995,6 +1000,115 @@ if (typeof require === 'undefined') {
     var require = ll.require;
 }
 
+define('./pattern', [], function (exports) {
+    var Pattern = {};
+
+    Pattern.unit = function unit(id) {
+        var type;
+        var value;
+        if (id === null) {
+            this.type = null;
+            this.value = null;
+        } else if (typeof id === 'function') {
+            this.type = id;
+            this.value = null;
+        } else if (id === undefined) {
+            return new Pattern.any();
+        } else {
+            this.type = id.constructor;
+            this.value = id;
+        }
+        if (id instanceof Array) {
+            var array = [];
+            id.forEach(function (ele) {
+                array.push(new unit(ele));
+            });
+            this.array = array;
+        }
+    };
+
+    Pattern.any = function any() {
+    };
+
+    Pattern.any.prototype.expect = function (arg) {
+        return true;
+    };
+
+    Pattern.unit.prototype.expect = function (arg) {
+        if (arg === null) {
+            return this.value === null;
+        } else {
+            if (this.value === null) {
+                return arg.constructor === this.type;
+            } else {
+                if (this.array !== undefined) {
+                    if (this.array.length === 0 && arg.length === 0) {
+                        return true;
+                    } else {
+                        if (this.array.length > arg.length) {
+                            return false;
+                        } else {
+                            for (var i = 0, length = this.array.length; i < length; i ++) {
+                                if (this.array[i].expect(arg[i]) === false) {
+                                    return false;
+                                }
+                            }
+                            return true;
+                        }
+                    }
+                } else {
+                    return this.value === arg && this.type === arg.constructor;
+                }
+            }
+        }
+    };
+
+    Pattern.PatternMatching = function PatternMatching(options) {
+        var opts = [];
+        options.forEach(function (rule) {
+            var args = [];
+            for (var i = 0, length = rule.length - 1; i < length; i ++) {
+                args.push(new Pattern.unit(rule[i]));
+            }
+            args.push(rule[rule.length - 1]);
+            //if (args.length > 1) {
+            opts.push(args);
+            //}
+        });
+        //console.log(opts);
+        return function () {
+            var result;
+            var args = arguments;
+            opts.some(function (rule) {
+                var flag = true;
+                for (var i = 0, length = rule.length - 1; i < length; i ++) {
+                    //rule.log(opt[i]);
+                    if (rule[i].expect(args[i]) === false) {
+                        flag = false;
+                    }
+                }
+                if (flag === true) {
+                    var func = rule[length];
+                    result = func.apply(null, args);
+                }
+                return flag;
+            });
+            return result;
+        }
+    };
+
+    exports.Pattern = Pattern;
+});
+if (typeof define === 'undefined') {
+    var define = function (ns, deps, func) {
+        func(exports);
+    }
+}
+
+if (typeof require === 'undefined') {
+    var require = ll.require;
+}
+
 
 define('./scope', ['./node'], function (exports) {
     var Node = require('./node').Node;
@@ -1082,9 +1196,10 @@ define('./util', [], function (exports) {
     var hexDigit = '0123456789abcdefABCDEF';
 
     var keywords = ['if', 'then', 'else', 'let'];
+    var typewords = ['Number', 'String', 'List', 'Boolean'];
 
     var punctuatorStart = '+-*/!=|&^~%<>';
-    var singlePunctuator = '[]{}(),:\\;$.';
+    var singlePunctuator = '[]{}(),@:\\;$.';
 
     Util.isDigit = function (ch) {
         return '0' <= ch && '9' >= ch;
@@ -1140,6 +1255,10 @@ define('./util', [], function (exports) {
 
     Util.isKeyword = function (id) {
         return keywords.indexOf(id) > -1;
+    };
+
+    Util.isTypewords = function (id) {
+        return typewords.indexOf(id) > -1;
     };
 
     exports.Util = Util;
